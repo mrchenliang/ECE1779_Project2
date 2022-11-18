6c0d95e1ef1851139063d669a1eb92b6007b6ac9
BrianQu <qujianning0401@163.com>
2022-11-18 17:31:39 -0500

    update config constants

--- A_2/frontend/constants.py
+++ A_2/frontend/constants.py
@@ -1,26 +1,20 @@
 import os, requests, json
-resp = requests.get("http://169.254.169.254/latest/user-data/")
-conf_dict = json.loads(resp.content.decode('utf-8'))
+resp = requests.get("http://169.254.169.254/latest/user-data")
+config = json.loads(resp.content.decode('utf-8'))
 
-db_config = {'user': 'admin',
-             'password': 'ece1779group2',
-             'host': 'briandatabase.cls58pggr43c.us-east-1.rds.amazonaws.com',
+aws_config = {
+  'aws_access_key_id': config['aws_access_key_id'],
+  'awss_secret_access_key': config['aws_secret_access_key']
+}
+
+db_config = {'user': config["MySQL_user"],
+             'password': config["MySQL_password"],
+             'host': config["MySQL_host"],
              'port': '3306',
              'database': 'briandatabase'}
-             
-# db_config = {'user': conf_dict["MYSQL_USER"],
-#              'password': conf_dict["MYSQL_PASSWORD"],
-#              'host': conf_dict["MYSQL_HOST"],
-#              'port': '3306',
-#              'database': 'briandatabase'}
 
 
 max_capacity = 10
 replacement_policy = 'Least Recently Used'
-
-aws_config = {
-  'aws_access_key_id': conf_dict['aws_access_key_id'],
-  'awss_secret_access_key': conf_dict['aws_secret_access_key']
-}
 
 IMAGE_FOLDER = os.path.dirname(os.path.abspath(__file__)) + 'main/frontend/static/images'
