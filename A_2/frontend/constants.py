08139d2ef94c27862fa90a1f2964a019a909dd89
BrianQu <qujianning0401@163.com>
2022-11-19 15:59:20 -0500

    update

--- A_2/frontend/constants.py
+++ A_2/frontend/constants.py
@@ -2,10 +2,10 @@ import os, requests, json
 resp = requests.get("http://169.254.169.254/latest/user-data")
 config = json.loads(resp.content.decode('utf-8'))
 
-aws_config = {
-  'aws_access_key_id': config['aws_access_key_id'],
-  'awss_secret_access_key': config['aws_secret_access_key']
-}
+# aws_config = {
+#   'aws_access_key_id': config['aws_access_key_id'],
+#   'awss_secret_access_key': config['aws_secret_access_key']
+# }
 
 db_config = {'user': config["MySQL_user"],
              'password': config["MySQL_password"],
