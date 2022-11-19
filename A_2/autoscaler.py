a98bacb1dfe6e8f74e4454a41a5fb598e731d5af
BrianQu <qujianning0401@163.com>
2022-11-19 01:36:52 -0500

    update autoscaler

--- A_2/autoscaler.py
+++ A_2/autoscaler.py
@@ -107,6 +107,57 @@ def get_memcache_policy():
     return None
 
 
+def auto_scaler():
+    global backend
+
+    while (True):
+        resp = requests.get(backend + "/getMemcachePoolConfig")
+        pool_params = json.loads(resp.content.decode('utf-8'))
+        print(pool_params)
+        if pool_params['mode'] == 'automatic':
+            # runs every minute
+            unstable_count, active_count = get_pool_ready_count()
+            # Unstable implies instances are stil starting, stopping or pending
+            print("Active Count " + str(active_count))
+            print("Unstable Count " + str(unstable_count))
+
+            if unstable_count == 0:
+                miss_rate = get_stats_logs()
+                print("Current Miss Rate: " + str(miss_rate))
+                # Miss rate is none, when no logs have been printed for this time period
+                if not miss_rate == None:
+                    cache_policy = get_memcache_policy()
+                    print(cache_policy)
+                    if not cache_policy == None:
+                        if miss_rate > cache_policy[1]:
+                            # Max Miss Rate, Scale up instances
+                            print("Scale up")
+                            expand_factor = cache_policy[3]
+                            max_startup = round(expand_factor * active_count) - active_count
+                            if max_startup + active_count > 8:
+                                # Start a max of 8 nodes
+                                max_startup = 8 - active_count
+                            
+                            for i in range(max_startup):
+                                print("Call startup node")
+                                requests.post(backend + '/startInstance')
+
+                        # Get current memcache count, then scale up max 8
+                        elif miss_rate < cache_policy[2]:
+                            # Min Miss Rate, Scale up instances
+                            print("Scale Down")
+                            shrink_factor = cache_policy[4]
+                            max_shutdown = active_count - round(shrink_factor * active_count)
+                            if  active_count - max_shutdown < 1:
+                                # Shutdown max of all but 1
+                                max_shutdown = active_count - 1
+                            
+                            for i in range(max_shutdown):
+                                print("Call shutdown node")
+                                requests.post(backend + '/stopInstance')
+        time.sleep(60)
+
+auto_scaler()
 
 
 
