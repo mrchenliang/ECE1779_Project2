c5b03f066ba643803fd10887eeceff4f78c0fa5c
BrianQu <qujianning0401@163.com>
2022-11-20 17:23:55 -0500

    fix active_count

--- A_2/backend/AWS_Log_operator.py
+++ A_2/backend/AWS_Log_operator.py
@@ -55,7 +55,6 @@ def create_log(message):
 
 def get_aggregate_statistics():
     global memcache_pool, pre_stats
-    active_node_count = 0
     statistics = {
         'key_count': 0,
         'size_count': 0,
@@ -80,20 +79,20 @@ def get_aggregate_statistics():
                 statistics['request_count'] += response_dict['request_count']
                 statistics['hit_rate'] += response_dict['hit_rate']
                 statistics['miss_rate'] += response_dict['miss_rate']
-                active_node_count += 1
+                statistics['active_count'] += 1
             except:
                 print("Get statistics error")
     
-    if active_node_count > 0:
+    if statistics['active_count'] > 0:
         #rel_hit_rate, rel_miss_rate = update_rates(statistics['hit_rate'], statistics['miss_rate'])
         
         statistics_cur = {
-            'key_count': statistics['key_count']/active_node_count,
-            'size_count': statistics['size_count']/active_node_count,
-            'request_count': statistics['request_count']/active_node_count,
-            'miss_rate': statistics['miss_rate']/active_node_count,
-            'hit_rate': statistics['hit_rate']/active_node_count,
-            'active_count': active_node_count
+            'key_count': statistics['key_count']/statistics['active_count'],
+            'size_count': statistics['size_count']/statistics['active_count'],
+            'request_count': statistics['request_count']/statistics['active_count'],
+            'miss_rate': statistics['miss_rate']/statistics['active_count'],
+            'hit_rate': statistics['hit_rate']/statistics['active_count'],
+            'active_count': statistics['active_count']
         }
 
         pre_stats['miss_rate'] = statistics['miss_rate']
