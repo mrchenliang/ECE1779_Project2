d8cdf6d935b6d4884bb90845a8734a58173f4943
BrianQu <qujianning0401@163.com>
2022-11-20 01:55:48 -0500

    fix error

--- A_2/managerapp/stat/stat_routes.py
+++ A_2/managerapp/stat/stat_routes.py
@@ -7,5 +7,5 @@ stat_routes = Blueprint("stat_routes", __name__)
 @stat_routes.route('/memcache_stats')
 def memcache_stats():
     stat_data = get_stat_logs()
-    stat_data = json.dumps(stat_data)
+    stat_data = json.dumps(stat_data, indent=4, sort_keys=True, default=str)
     return render_template('stat.html', stat_data=stat_data)
